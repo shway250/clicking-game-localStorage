51a0d2efdae86ae339b7d95adcb5bf79f518fbab
shway250 <jackson_duhon@yahoo.com>
2017-09-07 21:20:24 -0700

    finished game with local storage;

--- script.js
+++ script.js
@@ -2,29 +2,28 @@ var redScore = 0;
 var greenScore = 0;
 var redSquare = $('#red-div');
 var greenSquare = $('#green-div');
+var redHighScore = $("#redHighScore");
+var greenHighScore = $("#greenHighScore");
+localStorage.red;
+localStorage.green;
 
 function increaseScore(event){
   event.data.param1 += 1;
+  if(event.data.param2 === redSquare && event.data.param1 >= localStorage.red){
+    localStorage.red = event.data.param1;
+  }else if(event.data.param2 === greenSquare && event.data.param1 >= localStorage.green){
+    localStorage.green = event.data.param1;
+  }
+  redHighScore.text(localStorage.red);
+  greenHighScore.text(localStorage.green);
   event.data.param2.empty()
   event.data.param2.append("<h2>"+event.data.param1+"</h2>")
 }
 
 redSquare.click({param1: redScore, param2: redSquare}, increaseScore);
 greenSquare.click({param1: greenScore, param2: greenSquare}, increaseScore);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+redHighScore.text(localStorage.red);
+greenHighScore.text(localStorage.green);
 
 
 // redSquare.click(function(){
@@ -32,6 +31,7 @@ greenSquare.click({param1: greenScore, param2: greenSquare}, increaseScore);
 //   redSquare.empty()
 //   redSquare.append("<h2>"+redScore+"</h2>")
 // });
+
 // greenSquare.click({param1: "Hello", param2: "World"},function(){
 //   greenScore += 1;
 //   greenSquare.empty()
