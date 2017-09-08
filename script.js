66b792597c5fc7ae9a045f30757b3186f2fa2383
shway250 <jackson_duhon@yahoo.com>
2017-09-08 15:49:22 -0700

    added some notes

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ var redSquare = $('#red-div');
 var greenSquare = $('#green-div');
 var redHighScore = $("#redHighScore");
 var greenHighScore = $("#greenHighScore");
-localStorage.red;
-localStorage.green;
-
+//make sure local storage variable is initiated.
+//this is actually a pretty good use of the == instead of ===
+if(localStorage.yellow == false){
+  localStorage.yellow = 0;
+}
+if(localStorage.blue == false){
+  localStorage.blue = 0;
+}
 function increaseScore(event){
   event.data.param1 += 1;
   if(event.data.param2 === redSquare && event.data.param1 >= localStorage.red){
@@ -24,17 +29,3 @@ redSquare.click({param1: redScore, param2: redSquare}, increaseScore);
 greenSquare.click({param1: greenScore, param2: greenSquare}, increaseScore);
 redHighScore.text(localStorage.red);
 greenHighScore.text(localStorage.green);
-
-
-// redSquare.click(function(){
-//   redScore += 1;
-//   redSquare.empty()
-//   redSquare.append("<h2>"+redScore+"</h2>")
-// });
-
-// greenSquare.click({param1: "Hello", param2: "World"},function(){
-//   greenScore += 1;
-//   greenSquare.empty()
-//   greenSquare.append("<h2>"+greenScore+"</h2>")
-//   console.log(this);
-// });
